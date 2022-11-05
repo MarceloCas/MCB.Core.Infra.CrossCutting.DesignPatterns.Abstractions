b15fb12814908b071b94aa8af91f72052bdb8cef
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-11-05 02:07:38 -0300

    feat: add UnitOfWork abstractions

---
+++ src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.UnitOfWork;
+
+public interface IUnitOfWork
+{
+    Task<bool> ExecuteAsync(Func<CancellationToken, Task> handler, CancellationToken cancellationToken);
+    Task<bool> ExecuteAsync<TInput>(Func<TInput?, CancellationToken, Task> handler, TInput? input, CancellationToken cancellationToken);
+    Task<(bool success, TOutput? output)> ExecuteAsync<TInput, TOutput>(Func<TInput?, CancellationToken, Task<TOutput?>> handler, TInput? input, CancellationToken cancellationToken);
+}
