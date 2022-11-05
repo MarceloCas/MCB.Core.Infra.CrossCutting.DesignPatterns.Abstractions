47a3ef9e1b4707c5a7f1c96cdf5aefc8d9899d2c
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-11-05 01:29:03 -0300

    refact: add nullable support to input and output

--- src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/Resilience/IResiliencePolicy.cs
+++ src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/Resilience/IResiliencePolicy.cs
@@ -21,6 +21,6 @@ public interface IResiliencePolicy
     void OpenCircuitBreakerManually();
 
     Task<bool> ExecuteAsync(Func<CancellationToken, Task> handler, CancellationToken cancellationToken);
-    Task<bool> ExecuteAsync<TInput>(Func<TInput, CancellationToken, Task> handler, TInput input, CancellationToken cancellationToken);
-    Task<(bool success, TOutput output)> ExecuteAsync<TInput, TOutput>(Func<TInput, CancellationToken, Task<TOutput>> handler, TInput input, CancellationToken cancellationToken);
+    Task<bool> ExecuteAsync<TInput>(Func<TInput?, CancellationToken, Task> handler, TInput? input, CancellationToken cancellationToken);
+    Task<(bool success, TOutput? output)> ExecuteAsync<TInput, TOutput>(Func<TInput?, CancellationToken, Task<TOutput?>> handler, TInput? input, CancellationToken cancellationToken);
 }
