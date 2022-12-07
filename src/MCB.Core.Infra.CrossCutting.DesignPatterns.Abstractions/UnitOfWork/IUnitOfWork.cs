8221e5157c8e7a85bf1789d45f106b4aea1f6074
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-12-07 00:57:03 -0300

    refact: change ExecuteAsync return

--- src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/UnitOfWork/IUnitOfWork.cs
+++ src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/UnitOfWork/IUnitOfWork.cs
@@ -4,5 +4,5 @@ public interface IUnitOfWork
 {
     Task<bool> ExecuteAsync(Func<(bool openTransaction, CancellationToken cancellationToken), Task<bool>> handler, bool openTransaction, CancellationToken cancellationToken);
     Task<bool> ExecuteAsync<TInput>(Func<(TInput? input, bool openTransaction, CancellationToken cancellationToken), Task<bool>> handler, TInput? input, bool openTransaction, CancellationToken cancellationToken);
-    Task<(bool success, TOutput? output)> ExecuteAsync<TInput, TOutput>(Func<(TInput? input, bool openTransaction, CancellationToken cancellationToken), Task<(bool, TOutput?)>> handler, TInput? input, bool openTransaction, CancellationToken cancellationToken);
+    Task<(bool success, TOutput? output)> ExecuteAsync<TInput, TOutput>(Func<(TInput? input, bool openTransaction, CancellationToken cancellationToken), Task<(bool Success, TOutput? Output)>> handler, TInput? input, bool openTransaction, CancellationToken cancellationToken);
 }
