dd200c5720794ae645b621cafad341c990cc45e3
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2023-01-22 18:19:55 -0300

    feat: add feature flag manager

---
+++ src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/FeatureFlags/IFeatureFlagManager.cs
@@ -0,0 +1,14 @@
+﻿namespace MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.FeatureFlags;
+
+public interface IFeatureFlagManager
+{
+    bool GetFlag(string key);
+    Task<bool> GetFlagAsync(string key, CancellationToken cancellationToken);
+
+    void AddOrUpdateFlag(string key, bool value);
+    Task AddOrUpdateFlagAsync(string key, bool value, CancellationToken cancellationToken);
+
+    void RemoveFlag(string key);
+    Task RemoveFlagAsync(string key, CancellationToken cancellationToken);
+}
+
