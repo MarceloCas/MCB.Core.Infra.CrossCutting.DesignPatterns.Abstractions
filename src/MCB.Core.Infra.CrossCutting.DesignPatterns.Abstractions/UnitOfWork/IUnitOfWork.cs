ab0c9158d51d80ee8e74ca9a56a398820a948cdb
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2023-01-30 23:24:59 -0300

    feat: add IsBulkInsertOperation to UnitOfWork

--- src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/UnitOfWork/IUnitOfWork.cs
+++ src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/UnitOfWork/IUnitOfWork.cs
@@ -2,7 +2,7 @@
 
 public interface IUnitOfWork
 {
-    Task<bool> ExecuteAsync(Func<(bool OpenTransaction, CancellationToken CancellationToken), Task<bool>> handler, bool openTransaction, CancellationToken cancellationToken);
-    Task<bool> ExecuteAsync<TInput>(Func<(TInput? Input, bool OpenTransaction, CancellationToken CancellationToken), Task<bool>> handler, TInput? input, bool openTransaction, CancellationToken cancellationToken);
-    Task<(bool Success, TOutput? Output)> ExecuteAsync<TInput, TOutput>(Func<(TInput? Input, bool OpenTransaction, CancellationToken CancellationToken), Task<(bool Success, TOutput? Output)>> handler, TInput? input, bool openTransaction, CancellationToken cancellationToken);
+    Task<bool> ExecuteAsync(Func<(bool OpenTransaction, bool IsBulkInsertOperation, CancellationToken CancellationToken), Task<bool>> handler, bool openTransaction, bool isBulkInsertOperation, CancellationToken cancellationToken);
+    Task<bool> ExecuteAsync<TInput>(Func<(TInput? Input, bool OpenTransaction, bool IsBulkInsertOperation, CancellationToken CancellationToken), Task<bool>> handler, TInput? input, bool openTransaction, bool isBulkInsertOperation, CancellationToken cancellationToken);
+    Task<(bool Success, TOutput? Output)> ExecuteAsync<TInput, TOutput>(Func<(TInput? Input, bool OpenTransaction, bool IsBulkInsertOperation, CancellationToken CancellationToken), Task<(bool Success, TOutput? Output)>> handler, TInput? input, bool openTransaction, bool isBulkInsertOperation, CancellationToken cancellationToken);
 }
