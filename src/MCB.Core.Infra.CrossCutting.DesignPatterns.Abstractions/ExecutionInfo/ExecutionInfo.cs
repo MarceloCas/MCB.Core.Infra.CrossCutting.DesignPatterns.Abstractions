0e312e58309f4a71c68cfcc698a6eee925187b1d
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2023-02-23 23:28:55 -0300

    feat: add execution Info

---
+++ src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/ExecutionInfo/ExecutionInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.ExecutionInfo;
+public readonly record struct ExecutionInfo
+{
+    // Properties
+    public Guid CorrelationId { get; }
+    public Guid TenantId { get; }
+    public string ExecutionUser { get; }
+    public string SourcePlatform { get; }
+
+    // Constructors
+    public ExecutionInfo(
+        Guid correlationId,
+        Guid tenantId,
+        string executionUser,
+        string sourcePlatform
+    )
+    {
+        CorrelationId = correlationId;
+        TenantId = tenantId;
+        ExecutionUser = executionUser;
+        SourcePlatform = sourcePlatform;
+    }
+}
